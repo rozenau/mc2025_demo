09624d747546f6da6bf17f6dca82c92cf84be2f0
Michal Rozenau <rozenau@parasoft.com>
2025-04-10 11:38:21 +0200

    Implement main loop

--- main.c
+++ main.c
@@ -6,41 +6,41 @@
 #include <stdlib.h>
 
 
-// static void processMsg(const msg_t* msg)
-// {
-//     int32_t result = 0;
-//     switch (msg->kind) {
-//         case INT32:
-//             result = processInt(msg->data.i);
-//             break;
-//         case FLOAT:
-//             result = processFloat(msg->data.f);
-//             break;
-//         case VOID:
-//         	return;
-//         default:
-//             break;
-//     }
-//     printStatus(result);
-// }
+static void processMsg(const msg_t* msg)
+{
+    int32_t result = 0;
+    switch (msg->kind) {
+        case INT32:
+            result = processInt(msg->data.i);
+            break;
+        case FLOAT:
+            result = processFloat(msg->data.f);
+            break;
+        case VOID:
+        	return;
+        default:
+            break;
+    }
+    printStatus(result);
+}
 
 static void main_loop(void)
 {
-    // while (true) {
-    //     int32_t* i = getInt();
-    //     if (i) {
-    //         msg_t msg = createIntMsg(*i);
-    //         processMsg(&msg);
-    //     }
-    //     float* f = getFloat();
-    //     if (f) {
-    //         msg_t msg = createFloatMsg(*f);
-    //         processMsg(&msg);
-    //     }
-    //     if (!i && !f) {
-    //         return;
-    //     }
-    // }
+    while (true) {
+        int32_t* i = getInt();
+        if (i != NULL) {
+            msg_t msg = createIntMsg(*i);
+            processMsg(&msg);
+        }
+        float* f = getFloat();
+        if (f != NULL) {
+            msg_t msg = createFloatMsg(*f);
+            processMsg(&msg);
+        }
+        if (i == NULL && f == NULL) {
+            return;
+        }
+    }
 }
 
 int main(void)
